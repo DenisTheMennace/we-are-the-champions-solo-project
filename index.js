426bc13cf99d52b8c747d7817994224d2174b0cc
Denis Yurchenko <83593507+DenisTheMennace@users.noreply.github.com>
2024-03-18 22:40:30 -0400

    added more css, starting JS files

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // javascript
+
+const endorsementInput = document.getElementById("endorsement-input");
+const fromInput = document.getElementById("from-input");
+const toInput = document.getElementById("to-input");
+const publishBtn = document.getElementById("publish-btn");
+
+publishBtn.addEventListener("click", function () {
+  console.log(`${fromInput.value} has sent ${toInput.value} a message: "${endorsementInput.value}`);
+});
